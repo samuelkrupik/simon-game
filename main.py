9b773a113fa1eb2643ae3f29a13c5637283a8eba
Samuel Krupík <samo.krupik@gmail.com>
2021-05-13 00:53:29 +0200

    Pridaná ikona

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 import pygame as pg
 from src.game import Game
-from src import setup
+from src import setup, tools
 
 
 def main():
-    pg.mixer.init()
     pg.init()
     pg.display.set_mode(setup.SCREEN_SIZE)
     pg.display.set_caption(setup.CAPTION)
+    pg.display.set_icon(pg.image.load(tools.parse_path(setup.IMG_PATH, 'others', "icon.png")).convert_alpha())
     Game().main_loop()
     pg.quit()
     sys.exit()
